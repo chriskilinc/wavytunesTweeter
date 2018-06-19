b518e1c233032557d5c489adb6e74fcbf7947794
chriskilinc <chriskilinc@gmail.com>
2018-06-19 13:57:11 +0200

    Changed onEvent and composeTweet to work async

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ let main = () => {
   //  Event
   onEvent();
 }
-let onEvent = () =>{
+
+async function onEvent() {
+  //  This will wait until a tweet is composed
+  await composeTweet().then(tweet => {
+    tweetIt(tweet);
+  }).catch(err => {
+    //  Some Error 
+    console.log(err);
+  });
+
+  console.log("onEvent Finished.");
+}
+
+async function composeTweet() {
+  console.log(">  Composing Tweet");
+  //  Get all data to create a finished Status message
+  let finishedStatus = "I'm a tweet!";
+
   //  Compose Tweet
-  let composedTweet = composeTweet();
-  //  Post Tweet
-  tweetIt(composedTweet);
-}
-let composeTweet = () =>{
-  let fullStatus = "I'm a tweet!";
   let tweet = {
-    status: fullStatus
+    status: finishedStatus
   }
-  return {tweet}
+  return await tweet;
 }
+
 let tweetIt = (tweet) => {
   console.log(">  Tweeting");
   console.log(tweet);
