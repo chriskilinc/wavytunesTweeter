1afd7d50e93ddb2b08a4f64dfc10ccf6f8fca32c
chriskilinc <chriskilinc@gmail.com>
2018-06-19 13:45:41 +0200

    Added Function Structure

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+let main = () => {
+  //  Event
+  onEvent();
+}
+let onEvent = () =>{
+  //  Compose Tweet
+  let composedTweet = composeTweet();
+  //  Post Tweet
+  tweetIt(composedTweet);
+}
+let composeTweet = () =>{
+  let fullStatus = "I'm a tweet!";
+  let tweet = {
+    status: fullStatus
+  }
+  return {tweet}
+}
+let tweetIt = (tweet) => {
+  console.log(">  Tweeting");
+  console.log(tweet);
+}
+
+main();
